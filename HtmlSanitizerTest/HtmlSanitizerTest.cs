c53b67731e6841497eeca8b89d10ae58578f46ac
Chuck LU <chucklu2015@outlook.com>
2021-09-01 18:14:42 +0800

    test json xss

--- HtmlSanitizerTest/HtmlSanitizerTest.cs
+++ HtmlSanitizerTest/HtmlSanitizerTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace HtmlSanitizerTest
@@ -30,5 +31,32 @@ namespace HtmlSanitizerTest
             var html = @"\u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0031\u0029\u003E";
             CrossSiteScriptingValidation.ValidateDangerousString(html, 1);
         }
+
+        [Test]
+        public void Test20210901_004()
+        {
+            Employee employee = new Employee();
+            employee.FullName = "chucklu";
+            var result = JsonConvert.SerializeObject(employee);
+            Console.WriteLine(result);
+            //{"FullName":"chucklu"}
+        }
+
+        [Test]
+        public void Test20210901_005()
+        {
+            var html =
+                @"\u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0031\u0029\u003E";
+
+            var jsonString = "{ \"FullName\":\"" + html + "\"}";
+            //Console.WriteLine(jsonString);
+            var result = JsonConvert.DeserializeObject<Employee>(jsonString);
+            Console.WriteLine(result?.FullName);
+        }
+    }
+
+    public class Employee
+    {
+        public string FullName { get; set; }
     }
 }
