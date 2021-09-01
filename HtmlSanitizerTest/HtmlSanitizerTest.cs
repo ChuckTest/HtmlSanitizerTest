eddfa174afa1bf14887d23390b038d84e4bc6528
Chuck LU <chucklu2015@outlook.com>
2021-09-01 16:49:51 +0800

    add JavaScript encoded html

--- HtmlSanitizerTest/HtmlSanitizerTest.cs
+++ HtmlSanitizerTest/HtmlSanitizerTest.cs
@@ -21,7 +21,14 @@ namespace HtmlSanitizerTest
         {
             var html = @"<script>alert('xss')</script><div onload=""alert('xss')""";
             CrossSiteScriptingValidation.ValidateDangerousString(html, 1);
-            Console.WriteLine();
+        }
+
+        [Test]
+        public void Test20210901_003()
+        {
+            //<img src=x onerror=alert(1)>
+            var html = @"\u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0031\u0029\u003E";
+            CrossSiteScriptingValidation.ValidateDangerousString(html, 1);
         }
     }
 }
