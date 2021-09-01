cfa248e6744890fe5b08645247eda3b308996962
Chuck LU <chucklu2015@outlook.com>
2021-09-01 16:21:04 +0800

    add a test project

---
+++ HtmlSanitizerTest/HtmlSanitizerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HtmlSanitizerTest
+{
+    [TestFixture]
+    public class HtmlSanitizerTest
+    {
+        [Test]
+        public void Test20210901_001()
+        {
+            Console.WriteLine("hello world!");
+        }
+
+    }
+}
